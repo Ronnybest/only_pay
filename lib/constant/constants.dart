8dd35f1aa326773b47df7a1a86a35ae3f571cb83
Ronnybest <39735856+Ronnybest@users.noreply.github.com>
2023-05-24 20:43:09 +0600

    Update constants.dart

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -1,6 +1,1 @@
-// ignore: constant_identifier_names
-const MONGO_URL =
-    "mongodb+srv://danil123:danil123@fullstack.y3wqjdw.mongodb.net/OnlyPay?retryWrites=true&w=majority";
-// ignore: constant_identifier_names
-const COLLECTION_NAME = "users";
-const POSTS_DB = 'posts';
+
